f7351f2d5d821c9f86c2b044273840dfa39fe7a7
Ana Leite <silvaaraujoleite@gmail.com>
2018-12-12 18:22:10 -0200

    Continuação do back-end de login

--- src/br/com/fatec/blog/controller/LoginController.java
+++ src/br/com/fatec/blog/controller/LoginController.java
@@ -1,5 +1,48 @@
 package br.com.fatec.blog.controller;
 
-public class LoginController {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.fatec.blog.service.BaseService;
+import br.com.fatec.blog.service.BaseServiceImpl;
+
+public class LoginController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("login.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException{
+	String username = request.getParameter("username");
+	String password = request.getParameter("password");
+	
+	System.out.println(username + "::" + password);
+	String page = "login.jsp";
+	if(username.trim().length() >= 0 && username != null &&
+			password.trim().length() >= 0 && password != null) {
+		BaseService loginService.login(username, password);
+		
+		if(flag) {
+			System.out.println("Login feito com sucesso!");
+			request.setAttribute("username", username);
+			request.setAttribute("msg", "Login realizado!");
+			page = "home.jsp";
+		} else {
+			request.setAttribute("msg", "Usuário ou senha errado, tente novamente.");
+		}
+		} else {
+			request.setAttribute("msg", "Por favor, entre com o usuário e com a senha.");
+		}
+		request.getRequestDispatcher(page).include(request, response);
+	}
 }
+
+
--- src/br/com/fatec/blog/controller/RegistrationController.java
+++ src/br/com/fatec/blog/controller/RegistrationController.java
@@ -1,5 +1,52 @@
 package br.com.fatec.blog.controller;
 
-public class RegistrationController {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.fatec.blog.dao.BaseDao;
+import br.com.fatec.blog.dao.BaseDaoImpl;
+import br.com.fatec.blog.pojo.User;
+
+public class RegistrationController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public RegistrationController() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("userRegistration.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String msg = "As senhas devem ser as mesmas";
+		String page = "userRegistration.jsp";
+
+		if (request.getParameter("password").equals(request.getParameter("confPassword"))) {
+			User user = new User();
+			user.setUsername(request.getParameter("username"));
+			user.setPassword(request.getParameter("password"));
+			user.setFirstName(request.getParameter("firstName"));
+			user.setLastName(request.getParameter("lastName"));
+			user.setBirth(request.getParameter("birth"));
+			user.setEmailId(request.getParameter("emailId"));
+			user.setCell(request.getParameter("cell"));
+
+			System.out.println(user.toString());
+			BaseDao baseDao = new BaseDaoImpl();
+			msg = baseDao.register(user);
+			page = "login.jsp";
+		}
+
+		request.setAttribute("msg2", msg);
+		request.getRequestDispatcher(page).include(request, response);
+	}
 
 }
